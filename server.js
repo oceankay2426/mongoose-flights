fcec7abe0c2d5b9580c13b8003857e2389bd7563
oceankay2426 <travelkay24@gmail.com>
2024-04-17 15:28:16 -0500

    commit

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/flights', flightsRouter);
-app.use('/s', destinationsRouter);
+app.use('/', destinationsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
